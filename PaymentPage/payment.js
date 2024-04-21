623423fb7a48d8b73fe1fd5758faff0c5d22e38f
vidipsingh <112854574+vidipsingh@users.noreply.github.com>
2024-04-22 01:25:57 +0530

    Delete PaymentPage directory

--- PaymentPage/payment.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("Submit").onclick = () => {
-    alert("Payment Sucessfull");
-    window.location.href = "../index.html";
-};
-document.getElementById("close").addEventListener("click",function()    {
-        window.location.href = "../index.html";
-})
