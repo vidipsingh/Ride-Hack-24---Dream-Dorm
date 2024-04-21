4f8e62b509b23bed621581e984776a18eea602c0
vidipsingh <112854574+vidipsingh@users.noreply.github.com>
2024-04-21 23:13:39 +0530

    Update searchbox.js

--- components/searchbox.js
+++ components/searchbox.js
@@ -1,12 +1,12 @@
 
 let navComponent = `<div id="navbar" class="py-4 d-flex justify-content-between align-items-center">
 <div class="brand_logo">
-    <img src="https://res.cloudinary.com/stanza-living/image/upload/v1583405334/NewWebsite/Stanza_Living_Logo_3x.png"
+    <img src=""
         alt="">
 </div>
 <div class="nav_links">
     <a href="#" id="exlpore_residence_link">Explore Residences</a>
-    <a href="">
+    <a href="https://github.com/code735/stanzaLiving/blob/main/components/Screenshot_2024-04-20_132955-removebg-preview.png">
         Know More
         <span class="material-symbols-outlined">
             expand_more
@@ -74,7 +74,7 @@ let navComponent = `<div id="navbar" class="py-4 d-flex justify-content-between 
 let footerComponent = `<footer class="mt-4 pt-5 ps-5 pe-5 pb-2">
 <div class="d-flex justify-content-between">
     <div class="footer_brand_logo">
-        <img src="https://res.cloudinary.com/stanza-living/image/upload/w_182/v1652358946/Website%20v5/Logo/stanzalogo.png"
+        <img src="https://github.com/code735/stanzaLiving/blob/main/components/DREAM%20DORM-WHITE.png"
             alt="">
     </div>
     <div id="footer_links" class="d-none d-lg-flex justify-content-center">
@@ -84,35 +84,18 @@ let footerComponent = `<footer class="mt-4 pt-5 ps-5 pe-5 pb-2">
             <a href="#">Investor Relations</a>
             <a href="#">Media</a>
         </div>
-        <div class="d-flex flex-column">
-            <a href="#">Blogs</a>
-            <a href="#">FAQs</a>
-            <a href="#">Refer and Earn</a>
-            <a href="#">House Rules</a>
-        </div>
-        <div class="d-flex flex-column">
-            <a href="#">T&C</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Careers</a>
-            <a href="#">Contact Us</a>
-        </div>
-        <div class="d-flex flex-column">
-            <a href="#">COVID-19</a>
-            <a href="#">Refunds</a>
-            <a href="#">Partner with Us</a>
-            <a href="#">Cookie Policy</a>
-        </div>
+      
+       
     </div>
     <div id="social_links">
         <div class="d-flex justify-content-between align-items-center">
-            <a href=""><i class="bi bi-facebook"></i></a>
             <a href=""><i class="bi bi-linkedin"></i></a>
             <a href=""><i class="bi bi-instagram"></i></a>
         </div>
     </div>
 </div>
 <div class="copyright d-flex justify-content-between align-items-center pt-5">
-    <p class="copyright_l">Copyright © 2022 | All Rights Reserved by Dtwelve Spaces Pvt Ltd. | Sitemap</p>
+    <p class="copyright_l">Copyright © 2024 | All Rights Reserved. | Sitemap</p>
     <p class="copyright_r">Images shown are for representational purposes only. Amenities depicted may or may
         not form a part of
         that individual property.</p>
