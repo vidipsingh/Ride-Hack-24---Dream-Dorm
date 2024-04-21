f806ccd5fefd54fc3a6e8bce6ce05c157bcdfd5d
vidipsingh <112854574+vidipsingh@users.noreply.github.com>
2024-04-22 01:25:47 +0530

    Delete EndPage directory

--- EndPage/Endpage.js
+++
@@ -1,7 +0,0 @@
-let indData = JSON.parse(localStorage.getItem("individual"));
-
-console.log(indData.product.addressResponseDTO.displayAddress)
-console.log(indData.product.addressResponseDTO.line2)
-
-
-document.getElementById("location_text").textContent = indData.product.addressResponseDTO.displayAddress + " " + indData.product.addressResponseDTO.line2;
